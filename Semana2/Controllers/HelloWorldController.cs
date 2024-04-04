e56cd382fe13b9b1da9520ebbfd9ef0aa2b31b47
SebastianTripolone <seba99vte@gmail.com>
2024-04-04 16:28:52 -0300

    Semana 3

---
+++ Semana2/Controllers/HelloWorldController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace Semana2.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: /HelloWorld/ Reemplazar el método index:
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        //GET: /HelloWorld/Welcome/ Agregar este método:
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hola mi querido amigo " + name;
+            ViewData["NumTimes"] = numTimes;
+
+
+            return View();
+        }
+    }
+}
